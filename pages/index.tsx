0145dd397038234112cc57fb11a507a10e5756d3
idil <idil.iman1999@gmail.com>
2022-11-24 00:34:51 +0800

    fetch data from thirdweb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,77 @@
+import { BanknotesIcon, ClockIcon } from "@heroicons/react/24/outline";
+import {
+  useActiveListings,
+  useContract,
+  MediaRenderer,
+} from "@thirdweb-dev/react";
+import { ListingType } from "@thirdweb-dev/sdk";
 import Header from "../components/Header";
 
 const Home = () => {
+  const { contract } = useContract(
+    process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT,
+    "marketplace"
+  );
+
+  const { data: listings, isLoading: loadingListings } =
+    useActiveListings(contract);
+
+  console.log(listings);
   return (
-      <div>
-        <Header />
-      </div>
+    <div>
+      <Header />
+      <main className="max-w-6xl mx-auto p-2">
+        {loadingListings ? (
+          <p className="text-center animate-pulse text-blue-500">Loading</p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {listings?.map((listing) => (
+              <div
+                className="flex items-center cursor-pointer flex-col card hover:scale-105 transition-all duration-150 ease-out"
+                key={listing.id}
+              >
+                <div className="flex flex-1 pb-2">
+                  <MediaRenderer className="w-44" src={listing.asset.image} />
+                </div>
+
+                <div className="pt-2 space-y-4">
+                  <div>
+                    <h2 className="text-lg">{listing.asset.name}</h2>
+                    <hr />
+                  </div>
+
+                  <p>
+                    <span className="font-bold mr-1">
+                      {listing.buyoutCurrencyValuePerToken.displayValue}
+                    </span>
+                    {listing.buyoutCurrencyValuePerToken.symbol}
+                  </p>
+
+                  <div
+                    className={`flex items-center space-x-1 text-xs border w-fit p-2 rounded-lg text-white ${
+                      listing.type === ListingType.Direct
+                        ? "bg-blue-500"
+                        : "bg-red-500"
+                    }`}
+                  >
+                    <p>
+                      {listing.type === ListingType.Direct
+                        ? "Buy Now"
+                        : "Auction"}
+                    </p>
+                    {listing.type === ListingType.Direct ? (
+                      <BanknotesIcon className="h-4" />
+                    ) : (
+                      <ClockIcon className="h-4" />
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </main>
+    </div>
   );
 };
 
