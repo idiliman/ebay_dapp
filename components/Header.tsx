dfe75354265774c789005b2051d5f814faea20f5
idil <idil.iman1999@gmail.com>
2022-11-23 15:43:40 +0800

    done header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import Link from "next/link";
+import {
+  BellIcon,
+  ShoppingCartIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 
-type Props = {}
-
-function Header({}: Props) {
+export default function Header() {
+  const connectWithMetamask = useMetamask();
+  const disconnect = useDisconnect();
+  const address = useAddress();
   return (
-    <div>Header</div>
-  )
-}
+    <div className="max-w-6xl mx-auto">
+      <nav className="flex justify-between">
+        {/* Button/Right content */}
+        <div className="flex items-center space-x-2 text-sm">
+          {address ? (
+            <button onClick={disconnect} className="connectWalletBtn">
+              Hi, {address.slice(0, 4) + "..." + address.slice(-4)}
+            </button>
+          ) : (
+            <button onClick={connectWithMetamask} className="connectWalletBtn">
+              Connect Wallet
+            </button>
+          )}
 
-export default Header
+          {/*  */}
+          <p className="headerLink">Daily Deals</p>
+          <p className="headerLink">Help & Contact</p>
+        </div>
+
+        {/* Left content */}
+        <div className="flex items-center space-x-4 text-sm">
+          <p className="headerLink">Ship to</p>
+          <p className="headerLink">Sell</p>
+          <p className="headerLink">Watchlist</p>
+
+          <Link href="/addItem" className="flex items-center hover:link">
+            Add to inventory <ChevronDownIcon className="h-4" />
+          </Link>
+        </div>
+      </nav>
+
+      <hr className="mt-2"/>
+      
+      <section>
+        <div>
+          
+        </div>
+      </section>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div className="text-red-400">
-      <Header/>
-    </div>
+      <div>
+        <Header />
+      </div>
   );
 };
 
